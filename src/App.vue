f4b1b42d7b6eb349540d1461ce8ac5b0ae52ad03
J-Mahanty <jayatimahanty@gmail.com>
2025-08-21 11:48:07 +0530

    splash screen added, not functional

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <header class="header-wrapper">
-    <h1>Clock</h1>
-  </header>
 
+  <HomeView/>
+  <!-- <body>
+    <header class="header-wrapper">
+      <h1>Clock</h1>
+    </header> 
+  </body> -->
   
+
 </template>
 
 <style lang="scss" scoped> 
 
-.header-wrapper {
-  width: 90%;
-  justify-self: center;
-  margin: 20px;
-  padding: 8px;
+body {
+  background-image: linear-gradient( 90deg, #000000 , #5b5a5a, var(--base) );
 
-  h1{
-    font-family: "Akaya Telivigala";
-    font-size: 40px;
-    letter-spacing: 2px;
-    color : var(--base);
+  .header-wrapper {
+    width: 90%;
+    justify-self: center;
+    margin: 0 20px 20px;
+    padding: 20px 8px 8px;
+
+    h1{
+      font-family: "Akaya Telivigala";
+      font-size: 40px;
+      letter-spacing: 2px;
+      color : var(--base);
+    }
   }
 }
+
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,64 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+  var splashScreen = document.querySelector('.splash');
+  console.log(splashScreen);
+
+  function fadeOut(_event: any){
+    if(splashScreen){
+      setTimeout(() => {
+        splashScreen?.classList.add('hidden');
+      },1000)
+    }else{
+      alert("null?");
+    }
+  }
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="splash">
+    <div class="text-wrapper" @click="(event) => fadeOut(event)">
+      <h1>Clock</h1>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped> 
+  
+ .splash {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    
+    .text-wrapper{
+      display:flex;
+      position: relative;
+      background-color: var(--base);
+      margin: 100px;
+      width: 500px;
+      height: 500px;
+      border-radius: 500px;
+      justify-self: center;
+      justify-content: center;
+      cursor: pointer;
+      transition: ease-in-out 600ms;
+
+      h1{
+        text-align: center;
+        position: absolute;
+        padding: 180px 0;
+        font-family: "Akaya Telivigala";
+        font-size: 100px;
+        letter-spacing: 2px;
+        color : black
+      }
+    }
+    
+  }
+
+  .hidden {
+    transition: 0.5s;
+    display: none; 
+  }
+
+</style>
